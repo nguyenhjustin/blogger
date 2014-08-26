381c702150d240dcf7bd7378f7043627668e621f
Justin Nguyen <nguyenhjustin@yahoo.com>
2014-08-26 16:46:39 -0700

    Basic blogging functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController 
+	include ArticlesHelper
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article '#{@article.title}' created!"
+		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article '#{@article.title}' updated!"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		title = Article.find(params[:id]).title
+		flash.notice = "Article '" + title + "' deleted!"
+		Article.find(params[:id]).destroy()
+		redirect_to articles_path
+	end
+end
